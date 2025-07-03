85fd2082271292b5a44212e48410d60a0b798a46
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-07-03 16:33:56 +0400

    refactor: update NavBar styles for improved responsiveness and adjust background gradient in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,9 @@ const Home = () => {
           width={1440}
           height={1080}
         />
-        <div className='absolute inset-0 bg-gradient-to-b from-black/[0.9] from-10% via-transparent to-black to-95% -z-9'></div>
+        {/* <div className='absolute inset-0 bg-gradient-to-b from-black/[0.9] from-10% via-transparent to-black to-95% -z-9'></div> */}
+        <div className='absolute inset-0 bg-gradient-to-b bg-[radial-gradient(ellipse_at_center,_transparent_5%,_rgba(0,0,0,1)_95%,_rgba(0,0,0,0.7)_100%)] -z-9'></div>
+        {/* bg-[radial-gradient(ellipse_at_center,_transparent_40%,_rgba(0,0,0,0.7)_70%,_rgba(0,0,0,0.7)_100%)] */}
         <Hero />
       </main>
     </>
