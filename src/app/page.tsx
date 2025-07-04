b1d5e7f498dc2c6302adc9add19ece6f1ef71e75
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-07-04 08:04:53 +0400

    add download and play section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,21 @@ const Home = () => {
             <p className='text-lg text-gray-400'>Be the first to witness the extraordinary with our Exclusive Premieres. From Hollywood blockbusters to independent gems, catch the latest releases before they hit other platforms.</p>
           </div>
         </section>
+        <section className='px-[100px] py-20 bg-black backdrop-blur-md max-w-7xl mx-auto flex items-center gap-20'>
+          <div>
+            <h2 className='text-white text-5xl font-semibold text-center text-nowrap mb-6'>Download and Play</h2>
+            <p className='text-lg text-gray-400'>
+              No Wi-Fi? No problem! Download your go-to movies and watch them offline. Whether you're on a plane, in a remote cabin, or just lounging in your backyard, our Download and Play feature lets you take your favorite films with you, anytime, anywhere.
+            </p>
+          </div>
+          <Image
+            src="/download-and-play.png"
+            alt="Movie Poster"
+            width={580}
+            height={423}
+            className="h-auto rounded-lg shadow-md"
+          />
+        </section>
       </main>
     </>
   )
