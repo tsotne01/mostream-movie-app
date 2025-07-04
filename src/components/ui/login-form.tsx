28af79a1bee06c574364f55253c8c37ce6bada21
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-07-04 09:05:52 +0400

    feat(auth): implement login form with email and password fields

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -1,9 +1,22 @@
 'use client'
 import React from 'react'
+import InputField from './input-field'
+import Button from './button'
+import Link from 'next/link'
 
 const LoginForm = () => {
   return (
-    <div>login form</div>
+    <div className='w-full'>
+        <form className='flex flex-col gap-4'>
+            <InputField id='email' label="Email" type="email" placeholder='example@email.com'/>
+            <InputField id='password' label="Password" type="password" placeholder='Input Password'/>
+            <Button type='submit' variant='primary' disabled={false}>Sign In</Button>
+            <Button type='button' variant='secondary' className='border-1 border-[#383838] mb-8' beforeIcon={<>G</>}>Sign In with Google</Button>
+            <span className='text-center'>Don't have an account?
+                <Link href="/register" className='text-[#F14141]'> Sign Up</Link>
+            </span>
+        </form>
+    </div>
   )
 }
 
