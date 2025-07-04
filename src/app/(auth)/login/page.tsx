aa8e145587e7f5171c1ca28f6ec92931ad2b3863
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-07-04 09:09:41 +0400

    feat(auth): enhance login page with background image and overlay

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,19 @@
 import LoginForm from '@/components/ui/login-form'
 import Logo from '@/components/ui/logo'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
     return (
-        <main className='flex justify-center items-center h-screen'>
+        <main className='flex justify-center items-center h-screen relative'>
+            <Image
+                src="/login-background.png"
+                alt="Background Image"
+                layout="fill"
+                objectFit="cover"
+                className="-z-10 absolute top-0 left-0 w-full h-full object-cover"
+            />
+            <div className='absolute inset-0 bg-black/80 -z-9'></div>
             <div className='max-w-lg flex flex-col items-center'>
                 <Logo className='mb-8' />
                 <h1 className='text-white text-5xl leading-12 tracking-tight font-semibold text-center mb-6'>Access the Cinematic Realm with Elegance</h1>
