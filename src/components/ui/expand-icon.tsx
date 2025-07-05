1674d9b8f332f9eda9b0458fd54bb1f3bc3caf8c
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-07-05 13:04:25 +0400

    feat(faq): add FAQ section with collapsible items and improve home page layout

---
+++ src/components/ui/expand-icon.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+const ExpandIcon = () => {
+  return (
+    <Image src="/expand-icon.png" alt="Expand Icon" width={24} height={24} />
+  )
+}
+
+export default ExpandIcon
---
+++ src/components/ui/faq.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import FaqItem from './faq-item'
+
+const FAQ = () => {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <FaqItem
+                question="What is MoStream?"
+                answer="Mostream is not just a platform; it's your VIP pass to a universe of captivating content that transcends the ordinary. Say goodbye to the mundane and embrace the extraordinary."
+            />
+            <FaqItem
+                question="What Devices are Compatible?"
+                answer="Mostream is compatible with a wide range of devices, including smart TVs, streaming media players, tablets, and smartphones. You can also access Mostream through your web browser on a computer."
+            />
+            <FaqItem
+                question="Can I Download Movies for Offline Viewing?"
+                answer="Yes, MoStream allows you to download movies for offline viewing on supported devices. Simply look for the download option within the app."
+            />
+            <FaqItem
+                question='What Genres Do You Offer?'
+                answer='Mostream offers a diverse range of genres to cater to all tastes, including action, drama, comedy, horror, and more. You can easily browse our extensive library to find content that suits your preferences.'
+            />
+            <FaqItem
+                question='How Does Billing Work?'
+                answer='Mostream offers flexible billing options to suit your needs. You can choose between monthly and annual subscription plans, and you can cancel your subscription at any time. Billing is processed securely through our payment gateway.'
+            />
+            <FaqItem
+                question="What Devices are Compatible?"
+                answer="Mostream is compatible with a wide range of devices, including smart TVs, streaming media players, tablets, and smartphones. You can also access Mostream through your web browser on a computer."
+            />
+            <FaqItem
+                question="Can I Cancel My Subscription?"
+                answer="Yes, you can cancel your MoStream subscription at any time. Simply go to your account settings and follow the cancellation process. Your subscription will remain active until the end of the current billing cycle."
+            />
+        </div>
+    )
+}
+
+export default FAQ
